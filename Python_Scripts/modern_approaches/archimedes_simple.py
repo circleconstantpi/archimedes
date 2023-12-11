653c7a3b394f931134476c5248d43136c565e320
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2023-12-11 19:12:47 +0100

    Update archimedes_simple.py

--- Python_Scripts/modern_approaches/archimedes_simple.py
+++ Python_Scripts/modern_approaches/archimedes_simple.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-'''Modern iterative Archimedes algorithm using the insights of Snellius
-and the refinement from Dörrie.
-'''
 
 # Import the standard Python module math.
 import math
