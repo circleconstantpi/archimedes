4b5c353bfd70d0def7919f9eafca30f16020b0f2
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2023-12-11 10:53:25 +0100

    Add files via upload

--- Python_Scripts/modern_approaches/archimedes_recurrence_modified.py
+++ Python_Scripts/modern_approaches/archimedes_recurrence_modified.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-'''Modern iterative Archimedes algorithm using the refinement from Dörrie.'''
+'''Modern iterative Archimedes algorithm using the insights of Snellius
+and the refinement from Dörrie.
+'''
 
 # Import the standard Python module math.
 import math
 
+# Define the radius.
+r = 1
+
 # Define the start values.
-a0 = 2 * math.sqrt(3)  # half of outer perimeter
-b0 = 3                 # half of inner perimeter
+a0 = r * 2 * math.sqrt(3)  # half of outer perimeter
+b0 = r * 3                 # half of inner perimeter
 
 # Set the number of iterations.
-iteration = 4
+iteration = 7
 
 # Loop an iteration from 0 to 5 to get 5 values of Pi.
 for i in range(0, iteration+1):
@@ -22,12 +27,15 @@ for i in range(0, iteration+1):
         # Calculate the half of inner and outer perimeter.
         a1 = (2*a0*b0)/(a0 + b0)
         b1 = math.sqrt(b0*a1)
-        # Store the old values for the next loop.
-        a0 = a1
-        b0 = b1
+    # Store the old values for the next loop.
+    a0 = a1
+    b0 = b1
     # Calculate the refinement of inner and outer bound.
     b3 = (3*a1*b1)/(2*a1 + b1)
     a3 = (a1 * b1**2)**(1/3)
     # Calculate and print the Archimedes constant.
-    ac = (a3 + b3)/2
+    #ac = (a3 + b3)/2
+    ac = (1/r)*(a3 + 4*b3)/5
     print(ac)
+
+print("\n3.14159265358979323\n")
